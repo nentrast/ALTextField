96f9b59afdf35022f93fbaa6612e5ed3b11e1fa8
Alexandr Lobanov <lobanov92@gmail.com>
2020-03-12 11:25:47 +0200

    Initial Commit

---
+++ ALTextField/ALTextField.h
@@ -0,0 +1,19 @@
+//
+//  ALTextField.h
+//  ALTextField
+//
+//  Created by Alexandr Lobanov on 12.03.2020.
+//  Copyright © 2020 Alexandr Lobanov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ALTextField.
+FOUNDATION_EXPORT double ALTextFieldVersionNumber;
+
+//! Project version string for ALTextField.
+FOUNDATION_EXPORT const unsigned char ALTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ALTextField/PublicHeader.h>
+
+
